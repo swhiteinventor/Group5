e994e4aaf29dbe930f1a1e47af77d98f35c9d82f
mgulbin13 <matthewgulbin13@gmail.com>
2019-03-14 16:31:27 -0400

    Matt's change

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,10 @@
+package main.java;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Adam");
         System.out.println("hello");
-        System.out.println("Molly") ; 
+        System.out.println("Molly") ;
+        System.out.println("Matt");
     }
 }
